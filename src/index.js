22c483d15b45cb40d97db895fdbe5d9297f6b8f2
Charlie Spencer <crs992+charr757@gmail.com>
2020-09-22 23:45:38 -0400

    update to htmlFor

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <div>
-      <label for="name" className="label">Enter name</label>
+      <label htmlFor="name" className="label">Enter name</label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
         {buttonText.text}
